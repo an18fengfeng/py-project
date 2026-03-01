20ff5bf73cfc9287f82bd433be0e87c0277b6f22
an18fengfeng <2262009415@qq.com>
2026-03-01 21:12:46 +0800

    涉及数据类型，type函数验证，len函数索引

---
+++ 2.py
@@ -0,0 +1,24 @@
+s=2134958
+b="dsadwadsfx"
+a=1.1
+print(s)
+# print(len(s))      len函数用于字符串，列表，字典，不能用于整数
+print(len(b))
+# print(s(4))       索引只能给字符串，列表用  ！！！！！！索引一定是用中括号括起来的！！！！！！
+print(str(s)[3])       #可以转化类型进行表示
+print(b[5])          #索引可以用整数表示
+print(b[len(b)-1])     #索引位数可以用函数加运算表示
+#布尔类型    暂时先了解还没学
+b1= True
+b2= False
+#空类型        一样还没学
+c=None
+#数据类型验证(用type函数验证)
+print(type(s))
+print(type(str(s)))
+print(type(b))
+print(type(b1))
+print(type(c))
+#函数调用目前来看是   函数(变量)    这样表示的
+
+
